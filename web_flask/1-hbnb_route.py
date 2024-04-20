00782e2d12d11cc96af8eaf1862094b1a96f382a
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-04-21 01:22:46 +0200

    “commit”

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+from flask import Flask
+
+HBNB = Flask(__name__)
+
+
+@HBNB.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@HBNB.route("/hbnb", strict_slashes=False)
+def hbnb_HBNB():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    HBNB.run()
